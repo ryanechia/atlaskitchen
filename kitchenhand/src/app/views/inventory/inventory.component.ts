5d12f00ef98189a4ba14218c487c8d3b9e73d843
Ryane Chia <ryanechia@outlook.com>
2022-02-21 04:14:07 +0800

    simple outlet list page

--- kitchenhand/src/app/views/inventory/inventory.component.ts
+++ kitchenhand/src/app/views/inventory/inventory.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { InventoryService } from '../../services/inventory/inventory.service';
+import { Observable } from 'rxjs';
+import { OutletService } from '../../services/outlet/outlet.service';
+import { Router } from '@angular/router';
+import { Outlet } from '../../services/outlet/outlet.model';
 
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.scss']
+  styleUrls: [ './inventory.component.scss' ]
 })
 export class InventoryComponent implements OnInit {
 
-  constructor() { }
+  public outlets$: Observable<Outlet[]> | undefined;
 
-  ngOnInit(): void {
+  constructor(
+    private inventoryService: InventoryService,
+    private outletService: OutletService,
+    private router: Router
+  ) {
+  }
+
+  public ngOnInit(): void {
+    this.outlets$ = this.outletService.getOutlets();
+
+      // todo: can enhance to show stock health information within the outlet's card.
+      // .pipe(
+      // mergeMap((outlets) => from(outlets).pipe(
+      //     mergeMap((outlet) => this.inventoryService.getStocks(outlet, )
+      //     ),
+      //   toArray()
+      //   )
+      // )
+    // );
+  }
+
+  public outletClickedHandler(outlet: Outlet) {
+    this.router.navigate([`/outlets/${outlet.id}`]);
   }
 
 }
