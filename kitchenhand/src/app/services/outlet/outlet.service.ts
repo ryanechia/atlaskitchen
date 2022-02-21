eca2e7a0b443162122760f582247350503b9fd16
Ryane Chia <ryanechia@outlook.com>
2022-02-22 00:52:20 +0800

    connect outlet service to API

--- kitchenhand/src/app/services/outlet/outlet.service.ts
+++ kitchenhand/src/app/services/outlet/outlet.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Outlet } from './outlet.model';
 
 @Injectable({
@@ -12,14 +12,40 @@ export class OutletService {
     private http: HttpClient
   ) { }
 
-  public getOutlets(): Observable<Outlet[]> {
-    return this.http.get('').pipe(
-      map((result: any) => result.outlets),
-      map( outlets => outlets.map((outlet: any) => {
+  public getOutlet(outletId: number): Observable<Outlet> {
+    // @ts-ignore
+    return this.http.get(`http://localhost:3000/outlets/${outletId}`).pipe(
+      map((result: any) => result.outlet),
+      map((outlet: any) => {
         return {
           id: outlet.id,
           label: outlet.label,
+          menu: outlet.menu,
+          image: outlet.image,
           timeslots: outlet.time_slots.map((timeslot: any) => {
+            return {
+              id: timeslot.id,
+              startTime: timeslot.start_time,
+              endTime: timeslot.end_time
+            };
+          })
+        };
+      }),
+      // @ts-ignore
+      catchError(() => of({})), // don't terminate the observable. let subscription carry on.
+    );
+  }
+
+  public getOutlets(): Observable<Outlet[]> {
+    return this.http.get('http://localhost:3000/outlets').pipe(
+      map((result: any) => result.outlets),
+      map( outlets => outlets.map((outlet: Outlet) => {
+        return {
+          id: outlet.id,
+          label: outlet.label,
+          menu: outlet.menu,
+          image:outlet.image,
+          timeslots: outlet.timeslots.map((timeslot: any) => {
             return {
               id: timeslot.id,
               startTime: timeslot.start_time,
