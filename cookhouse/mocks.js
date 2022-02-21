220efd6f8cca32c7d52c5941f20deda5316a67e5
Ryane Chia <ryanechia@outlook.com>
2022-02-22 00:34:38 +0800

    add mock data

---
+++ cookhouse/mocks.js
@@ -0,0 +1,61 @@
+
+export const modifiers = [
+  {
+    id: 1,
+    label: 'least spicy'
+  },
+  {
+    id: 2,
+    label: 'normal spicy'
+  },
+  {
+    id: 3,
+    label: 'more spicy'
+  },
+];
+
+export const modifierGroups = [
+  {
+    id: 1,
+    label: 'Spicy Level',
+    modifiers: [...modifiers]
+  }
+];
+
+export const menuItems = [
+  {
+    id: 1,
+    start_time: new Date(),
+    end_time: new Date(),
+    modifierGroups: [...modifierGroups]
+  }
+];
+
+export const sections = [
+  {
+    id: 1,
+    title: 'Appetizers',
+    items: [...menuItems]
+  }
+];
+
+export const menus = [
+  {
+    id: 1,
+    label: 'All Day',
+    sections
+  }
+];
+
+export const timeslots = [
+  {
+    id: 1,
+    start_time: new Date('2022-02-21T10:00:00.000Z'),
+    end_time: new Date('2022-02-21T11:00:00.000Z')
+  },
+  {
+    id: 2,
+    start_time: new Date('2022-02-21T11:00:00.000Z'),
+    end_time: new Date('2022-02-21T12:00:00.000Z')
+  }
+]
